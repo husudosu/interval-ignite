03b83a4dec573de14237fc067378b0afb34c6218
Ferenc Nánási <husudosu94@gmail.com>
2024-03-24 11:17:22 +0100

    React-navigation initailized

--- App.tsx
+++ App.tsx
@@ -3,6 +3,10 @@ import {useColorScheme} from 'react-native';
 import {PaperProvider} from 'react-native-paper';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {HomeView} from './views/HomeView';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -13,7 +17,11 @@ function App(): React.JSX.Element {
 
   return (
     <PaperProvider>
-      <HomeView />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeView} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
