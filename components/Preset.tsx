7608edf069fa5f20d98de943265f14a58f16d66b
Ferenc Nánási <husudosu94@gmail.com>
2024-03-29 08:13:56 +0100

    Preset component UI improved

--- components/Preset.tsx
+++ components/Preset.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Button, Text} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
+import {Button, Text, Card} from 'react-native-paper';
 import {IPreset} from '../utils/settings';
 
 interface Props {
@@ -11,8 +11,6 @@ interface Props {
 
 export const Preset = ({preset, navigation, onDelete}: Props) => {
   const onStart = () => {
-    console.log('Start');
-    console.log('Interval seconds', preset.intervalSeconds);
     navigation.replace('Countdown', {
       intervalLength:
         parseInt(preset.intervalMinutes, 10) * 60 +
@@ -24,13 +22,57 @@ export const Preset = ({preset, navigation, onDelete}: Props) => {
 
   return (
     <View>
-      <Text>Preset name: {preset.presetName}</Text>
-      <Text>Minutes: {preset.intervalMinutes.toString()}</Text>
-      <Text>Seconds: {preset.intervalSeconds.toString()}</Text>
-      <Text>Sets:{preset.intervalSets.toString()}</Text>
-      <Text>Rest: {preset.intervalRestLength.toString()}</Text>
-      <Button onPress={onStart}>Start</Button>
-      <Button onPress={() => onDelete(preset.id)}>Delete</Button>
+      <Card>
+        <Card.Content style={style.cardContent}>
+          <View style={style.titleContainer}>
+            <Text variant="titleLarge">{preset.presetName}</Text>
+            <Button onPress={() => onDelete(preset.id)}>Delete</Button>
+          </View>
+          <Text variant="bodyMedium">
+            <Text style={{fontWeight: 'bold'}} variant="bodyMedium">
+              Interval:
+            </Text>
+            {preset.intervalMinutes}:{preset.intervalSeconds}
+          </Text>
+          <Text variant="bodyMedium">
+            <Text style={{fontWeight: 'bold'}} variant="bodyMedium">
+              Sets:
+            </Text>
+            {preset.intervalSets}
+          </Text>
+          <Text variant="bodyMedium">
+            <Text style={{fontWeight: 'bold'}} variant="bodyMedium">
+              Rest:
+            </Text>
+            {preset.intervalRestLength} seconds
+          </Text>
+        </Card.Content>
+        <Card.Actions style={style.cardActions}>
+          <Button onPress={onStart} style={style.buttonStart}>
+            Start
+          </Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
 };
+
+export const style = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  cardContent: {
+    flexDirection: 'column',
+    gap: 8,
+  },
+  cardActions: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  buttonStart: {
+    width: '100%',
+  },
+});
